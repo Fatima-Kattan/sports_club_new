1ee95048863ce5f5854a29646069a7f7290b52f3
Fatima-Kattan <fatimaqattan959@gmail.com>
2025-11-02 10:23:45 +0300

    create data base

---
+++ database/migrations/2025_11_02_065916_create_activities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->text('image');
+            $table->boolean('is_active')->default(true);
+            $table->boolean('free_time')->default(true);
+            $table->enum('level',['beginner','intermediate','advanced'])->default('beginner');
+            $table->foreignId('facility_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
---
+++ database/migrations/2025_11_02_070043_create_employees_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->unsignedBigInteger('mgr_id');
+            $table->foreign('mgr_id')->references('id')->on('employees')->onDelete('restrict');
+            $table->decimal('salary', 8, 2);
+            $table->string('email')->unique();
+            $table->string('position');
+            $table->string('phone_number')->unique();
+            $table->string('specialization')->nullable();
+            $table->time('working_hours_start');
+            $table->time('working_hours_end');
+            $table->integer('years_of_experience')->default(0);
+            $table->enum('role', ['coach', 'employee'])->default('employee');
+            $table->date('hire_date');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
