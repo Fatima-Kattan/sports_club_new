756eb74a2ab7aba36290d3623c2e905dd067f9d0
Fatima-Kattan <fatimaqattan959@gmail.com>
2025-11-08 11:55:10 +0300

    Adding password filed

--- auth-system/database/migrations/0001_01_01_000000_create_users_table.php
+++ auth-system/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();
-            $table->date('birth_date');
-            $table->enum('gender', ['female','male'])->default('male');
-            $table->boolean('is_admin')->default(false);
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
 
