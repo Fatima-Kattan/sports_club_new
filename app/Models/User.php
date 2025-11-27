dc3e580054098e0a385516c0911671845d7500a6
Rama-abwdan <ramaabwdan06@gmail.com>
2025-11-27 14:49:59 +0300

    install Laravel Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 // use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens,HasFactory,Notifiable;
 
     protected $fillable = [
         'full_name', 
